190e61229fbeda9794fb24f413c6aa05449d695d
danag <le_bonanni@yahoo.com.ar>
2018-04-22 20:46:04 -0300

    add authentication service basic logic

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -1,5 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+export class User {
+  name: string;
+  email: string;
+ 
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+}
 
 /*
   Generated class for the AuthServiceProvider provider.
@@ -10,8 +21,47 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthServiceProvider {
 
+  currentUser: User;
+
   constructor(public http: HttpClient) {
     console.log('Hello AuthServiceProvider Provider');
   }
 
+  public login(credentials) {
+    if (credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials");
+    } else {
+      return Observable.create(observer => {
+        // At this point make a request to your backend to make a real check!
+        let access = (credentials.password === "pass" && credentials.email === "email");
+        this.currentUser = new User('Simon', 'saimon@devdactic.com');
+        observer.next(access);
+        observer.complete();
+      });
+    }
+  }
+
+  public register(credentials) {
+    if (credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials");
+    } else {
+      // At this point store the credentials to your backend!
+      return Observable.create(observer => {
+        observer.next(true);
+        observer.complete();
+      });
+    }
+  }
+
+  public getUserInfo() : User {
+    return this.currentUser;
+  }
+ 
+  public logout() {
+    return Observable.create(observer => {
+      this.currentUser = null;
+      observer.next(true);
+      observer.complete();
+    });
+  }
 }
