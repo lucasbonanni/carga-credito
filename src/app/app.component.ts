0c876f4cd1ad9f7b9b2c46d3586b888cbc29e17a
danag <le_bonanni@yahoo.com.ar>
2018-05-01 12:24:46 -0300

    parte del commit anterior que no subió

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform, ModalController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 
 
 @Component({
@@ -11,7 +12,12 @@ export class MyApp {
   splash: boolean = true;
   rootPage:any = 'LoginPage';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,modalCtrl: ModalController) {
+  constructor(
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen,
+    modalCtrl: ModalController,
+    private auth: AuthServiceProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,5 +25,18 @@ export class MyApp {
       splashScreen.hide();
       setTimeout(() => this.splash = false, 4000);
     });
+    this.auth.afAuth.authState
+    .subscribe(
+      user => {
+        if (user) {
+          this.rootPage = 'HomePage';
+        } else {
+          this.rootPage = 'LoginPage';
+        }
+      },
+      () => {
+        this.rootPage = 'LoginPage';
+      }
+    );
   }
 }
