efc0be4f94ef75db8c8bff296fe664cb5cb9895e
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-05-13 23:57:12 -0300

    se agrega la logica de carga de credito y la pantalla al home de la aplicación

--- src/models/credit-model.ts
+++ src/models/credit-model.ts
@@ -2,4 +2,20 @@ export class CreditModel {
     public displayName:string;
     public creditCode:string;
     public date:string;
+    public amount: number;
 }
+
+export const AMOUNTS = [
+    {
+      code: '8c95def646b6127282ed50454b73240300dccabc',
+      amount: 10,
+    },
+    {
+      code: 'ae338e4e0cbb4e4bcffaf9ce5b409feb8edd5172',
+      amount: 50,
+    },
+    {
+      code: '2786f4877b9091dcad7f35751bfcf5d5ea712b2f',
+      amount: 100,
+    }
+  ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,52 @@
-import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, IonicPage, AlertController, Alert } from 'ionic-angular';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { User } from 'firebase';
+import { CreditModel, AMOUNTS } from '../../models/credit-model';
+import { CreditDataProvider } from '../../providers/credit-data/credit-data';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-  username = '';
-  email = '';
-  constructor(private nav: NavController, private auth: AuthServiceProvider) {
-    let info = this.auth.getUserInfo();
-    this.username = info.email;
-    this.email = info.email;
+export class HomePage implements OnInit {
+
+  displayName: string = '';
+  user: User;
+  totalAmount: number = 0;
+
+  public credit: Array<CreditModel> = [];
+  public amount: number;
+
+  constructor(
+    private nav: NavController,
+    private auth: AuthServiceProvider,
+    private creditService: CreditDataProvider,
+    private barcodeScanner: BarcodeScanner,
+    private alertCtrl: AlertController) {
+  }
+
+  ngOnInit(): void {
+    this.user = this.auth.getUserInfo();
+    this.displayName = this.user.displayName;
+    this.creditService.getCredit()
+      .subscribe(
+        transactions => {
+          console.log(transactions);
+          this.credit = transactions.filter(value => value.displayName === this.displayName);
+          for (let index = 0; index < this.credit.length; index++) {
+            this.totalAmount += this.credit[index].amount;
+          }
+          console.log(this.totalAmount);
+        },
+        error => console.log(error),
+        () => {
+
+        }
+      );
+      console.log(this.totalAmount);
   }
 
   public logout() {
@@ -22,11 +55,45 @@ export class HomePage {
     });
   }
 
-  public chargeCredit(){
-    this.nav.push('CargaCreditoPage');
+  public chargeCredit() {
+    let alert :Alert;
+    this.barcodeScanner.scan().then(result => {
+      const exist = AMOUNTS.filter(value => value.code.toLowerCase() === result.text.trim().toLowerCase()).length > 0;
+      const isNew = this.credit.filter(value => value.creditCode === result.text.trim().toLowerCase()).length === 0;
+      if (exist && isNew) {
+        let credit = new CreditModel();
+        const date = new Date();
+        credit.amount = AMOUNTS.filter(value => value.code === result.text.trim().toLowerCase())[0].amount;
+        credit.creditCode = result.text.trim().toLowerCase();
+        credit.displayName = this.displayName;
+        credit.date = date.toString();
+        this.creditService.addCredit(credit);
+      }
+      else if (!exist) {
+        alert = this.alertCtrl.create({
+          title: 'Advertencia!',
+          subTitle: 'El codigo no es un codigo válido',
+          buttons: [
+            {
+              text: 'Aceptar',
+              role: 'cancel'
+            }]
+        });
+      }
+    }).catch(error => {
+      alert = this.alertCtrl.create({
+        title: 'Advertencia!',
+        subTitle: error,
+        buttons: [
+          {
+            text: 'Aceptar',
+            role: 'cancel'
+          }]
+      });
+    });
   }
 
-  public accountStatus(){
+  public accountStatus() {
     this.nav.push('CuentaPage');
   }
 
