d83a38522bebfafd4e19bde85c7869add6499b93
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-05-13 23:55:38 -0300

    se agrega el servicio de para manejar los datos de firebase

--- src/providers/credit-data/credit-data.ts
+++ src/providers/credit-data/credit-data.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { CreditModel } from '../credit/credit';
+import { Observable } from 'rxjs/Observable';
+import { CreditModel } from '../../models/credit-model';
 
 /*
   Generated class for the CreditDataProvider provider.
@@ -12,16 +13,18 @@ import { CreditModel } from '../credit/credit';
 export class CreditDataProvider {
 
   creditRef: AngularFireList<CreditModel>;
+  transactions: Observable<CreditModel[]>;
   constructor(private db: AngularFireDatabase) {
-    this.creditRef = this.db.list<CreditModel>('votacion');
+    this.creditRef = this.db.list<CreditModel>('credito');
+    this.transactions = this.creditRef.valueChanges();
   }
 
   public addCredit(credit: CreditModel) {
     this.creditRef.push(credit);
   }
 
-  public getCredit(displayName: string) {
-    return this.creditRef.stateChanges();
+  public getCredit():Observable<CreditModel[]> {
+    return this.transactions;
   }
 
 }
