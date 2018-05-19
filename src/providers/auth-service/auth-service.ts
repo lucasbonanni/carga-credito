b94c887f0685facad2748fb7b8a2b2beeedeb09f
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-05-19 00:48:15 -0300

    se agrega mostrar los mensajes de errores

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { User, AuthProvider } from '@firebase/auth-types';
 import * as firebase from 'firebase/app';
+import { ToastController } from 'ionic-angular';
 
 // export class User {
 //   name: string;
@@ -25,7 +26,7 @@ export class AuthServiceProvider {
 
   user: User;
   access: boolean;
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth,private toastCtrl: ToastController) {
     afAuth.authState.subscribe(user => {
       this.user = user;
     });
@@ -50,36 +51,44 @@ export class AuthServiceProvider {
   }
 
   signInWithGoogle() {
-		console.log('Sign in with google');
-		return this.oauthSignIn(new firebase.auth.GoogleAuthProvider());
-}
+    console.log('Sign in with google');
+    return this.oauthSignIn(new firebase.auth.GoogleAuthProvider());
+  }
 
-signInWithGithub() {
-  console.log('Sign in with github');
-  return this.oauthSignIn(new firebase.auth.GithubAuthProvider());
-}
+  signInWithGithub() {
+    console.log('Sign in with github');
+    return this.oauthSignIn(new firebase.auth.GithubAuthProvider());
+  }
 
-private oauthSignIn(provider: AuthProvider) {
-	if (!(<any>window).cordova) {
-		return this.afAuth.auth.signInWithPopup(provider);
-	} else {
-		return this.afAuth.auth.signInWithRedirect(provider)
-		.then(() => {
-			return this.afAuth.auth.getRedirectResult().then( result => {
-				// This gives you a Google Access Token.
-				// You can use it to access the Google API.
-				let token = result.credential.accessToken;
-				// The signed-in user info.
-				let user = result.user;
-				console.log(token, user);
-			}).catch(function(error) {
-				// Handle Errors here.
-				alert(error.message);
-			});
-		});
-	}
-}
-  
+  private oauthSignIn(provider: AuthProvider) {
+    if (!(<any>window).cordova) {
+      return this.afAuth.auth.signInWithPopup(provider);
+    } else {
+      return this.afAuth.auth.signInWithRedirect(provider)
+        .then(() => {
+          return this.afAuth.auth.getRedirectResult().then(result => {
+            // This gives you a Google Access Token.
+            // You can use it to access the Google API.
+            let token = result.credential.accessToken;
+            // The signed-in user info.
+            let user = result.user;
+            console.log(token, user);
+          }).catch(function (error) {
+            // Handle Errors here.
+            this.showMessage('Verifique sus credenciales');
+          });
+        });
+    }
+  }
+
+  showMessage(text: string) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+  }
+
   public getUserInfo(): User {
     return this.user;
   }
